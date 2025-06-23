8c6bc8295b17afab1780f42bac6dac187f9dbb5f
Arjun <natharjun028@gmail.com>
2025-06-23 22:11:12 +0530

    chore(asset-route) : added the updateStatus route

--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {createAsset , getAllAssets , getAssetById , assign, returnAsset} = require("../controllers/assetController");
+const {createAsset , getAllAssets , getAssetById , assign, returnAsset ,updateAssets} = require("../controllers/assetController");
 const { verifyToken } = require("../middleware/verifytoken");
 const { allowRoles } = require("../middleware/checkrole");
 
-// For adding a new asset
+// Routes
 router.post("/add", verifyToken, allowRoles("admin", "IT"), createAsset);
 router.get("/getall", verifyToken, allowRoles("admin", "IT"), getAllAssets);
 router.get("/:id", verifyToken, allowRoles("admin", "IT"), getAssetById);
 router.put("/assign", verifyToken, allowRoles("admin", "IT"), assign);
 router.put("/retrieve", verifyToken, allowRoles("admin", "IT"), returnAsset);
+router.put("/update", verifyToken, allowRoles("admin", "IT"), updateAssets);
+
 
 
 module.exports = router;
