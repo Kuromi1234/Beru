e49c4d83ce70db66783393620949ac3ddcbb266e
Arjun <natharjun028@gmail.com>
2025-06-27 14:29:10 +0530

    feat(routes):added the route for the psswwd-reset controller

---
+++ backend/routes/passwordRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { requestReset, resetPassword } = require("../controllers/passwordController");
+router.post("/request-reset", requestReset);
+router.post("/reset-password", resetPassword);
+
+module.exports = router;
