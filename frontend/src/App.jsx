e7e27975186e3b79fab95579d0bfa94168478291
Arjun <natharjun028@gmail.com>
2025-07-05 00:50:05 +0530

    feat: add routes for Login, ForgotPassword, VerifyOTP, and ResetPassword pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Register from './pages/register';
-// add Login, ForgotPassword, Dashboard, etc as you build them
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/forgotPassword";
+import Verifyotp from "./pages/VerifyOTP";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/ForgotPassword" element={< ForgotPassword/>} />
+      <Route path="/VerifyOTP" element={< Verifyotp/>} />
+      <Route path="/ResetPassword" element={< ResetPassword/>} />
     </Routes>
   );
 };
