0d2cdb5fa7f6c0e8aafa3a0d4bcb76502183136b
Arjun <natharjun028@gmail.com>
2025-07-04 17:06:31 +0530

    feat: add boxShadow and colors configuration to Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -5,6 +6,14 @@ export default {
       fontFamily: {
         signature: ['"Luxurious Script"', "cursive"],
       },
+      boxShadow: {
+        glassy: "0 0 40px rgba(255,255,255,0.05)",
+      },
+      colors: {
+      primary: '#A259FF',
+      secondary: '#302b63',
+      darkNavy: '#0f0c29',
+    },
     },
   },
   plugins: [],
