baba381839669d921d2a3154a52501650b6a919f
Arjun <natharjun028@gmail.com>
2025-07-12 11:22:07 +0530

    feat : added the route for stats

--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {createAsset , getAllAssets , getAssetById , assign, returnAsset ,updateAssets, deleteasset , UserAssets} = require("../controllers/assetController");
+const {createAsset , getAllAssets , getAssetById , assign, returnAsset ,updateAssets, deleteasset , UserAssets , assetStats} = require("../controllers/assetController");
 const { verifyToken } = require("../middleware/verifytoken");
 const { allowRoles } = require("../middleware/checkrole");
 
 // Routes
 router.post("/add", verifyToken, allowRoles("admin", "IT"), createAsset);
+router.get("/stats", verifyToken, allowRoles("admin"), assetStats);
 router.get("/getall", verifyToken, allowRoles("admin", "IT"), getAllAssets);
 router.get("/:id", verifyToken, allowRoles("admin", "IT"), getAssetById);
 router.put("/assign", verifyToken, allowRoles("admin", "IT"), assign);
