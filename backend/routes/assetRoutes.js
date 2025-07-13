b3a1cefb2c86ed1417b6fa5c586aeb876ee98178
Arjun <natharjun028@gmail.com>
2025-07-13 18:35:12 +0530

    refactor : restructured the route code for better routing flow

--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {createAsset , getAllAssets , getAssetById , assign, returnAsset ,updateAssets, deleteasset , UserAssets , assetStats} = require("../controllers/assetController");
+const {createAsset , getAllAssets , getAssetById , assign, returnAsset ,updateAssets, deleteasset , UserAssets , getDashboardStats} = require("../controllers/assetController");
 const { verifyToken } = require("../middleware/verifytoken");
 const { allowRoles } = require("../middleware/checkrole");
 
 // Routes
 router.post("/add", verifyToken, allowRoles("admin", "IT"), createAsset);
-router.get("/stats", verifyToken, allowRoles("admin"), assetStats);
+router.get("/stats", verifyToken, allowRoles("admin"), getDashboardStats);
 router.get("/getall", verifyToken, allowRoles("admin", "IT"), getAllAssets);
-router.get("/:id", verifyToken, allowRoles("admin", "IT"), getAssetById);
 router.put("/assign", verifyToken, allowRoles("admin", "IT"), assign);
 router.put("/retrieve", verifyToken, allowRoles("admin", "IT"), returnAsset);
 router.put("/update", verifyToken, allowRoles("admin", "IT"), updateAssets);
+router.get("/:id", verifyToken, allowRoles("admin", "IT"), getAssetById);
 router.delete("/delete/:id", verifyToken, allowRoles("admin"), deleteasset);
 router.get("/user/:id", verifyToken, allowRoles("admin","IT"), UserAssets);
 
