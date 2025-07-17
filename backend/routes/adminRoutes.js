5748b33b901ca7a7cefabc0fae93755e4271a0c0
Arjun <natharjun028@gmail.com>
2025-07-18 02:29:42 +0530

    feat : route addition for deleteuser

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {adminResetUserPassword} = require("../controllers/adminController");
+const {adminResetUserPassword , deleteUser} = require("../controllers/adminController");
 
 const { verifyToken } = require("../middleware/verifytoken");
 const { allowRoles } = require("../middleware/checkrole"); 
 
 // Only admins can reset passwords
 router.put("/adminpsswd/:id", verifyToken, allowRoles("admin"), adminResetUserPassword);
-
+router.delete("/deleteuser/:id", verifyToken, allowRoles("admin"), deleteUser);
 
 module.exports = router;
