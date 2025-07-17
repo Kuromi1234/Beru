4adbe72c21ee25c0150baeb22ca0295317d0f57f
Arjun <natharjun028@gmail.com>
2025-07-17 23:00:43 +0530

    fix : proper routes implemented for finding user by id

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const adminController = require("../controllers/adminController");
+const {adminResetUserPassword} = require("../controllers/adminController");
 
 const { verifyToken } = require("../middleware/verifytoken");
 const { allowRoles } = require("../middleware/checkrole"); 
 
 // Only admins can reset passwords
-router.put("/adminpsswd", verifyToken, allowRoles("admin"), adminController.adminResetUserPassword);
+router.put("/adminpsswd/:id", verifyToken, allowRoles("admin"), adminResetUserPassword);
 
 
 module.exports = router;
