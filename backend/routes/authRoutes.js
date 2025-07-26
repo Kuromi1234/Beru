3e9944240a710acd0fd3215541af58551cae0c18
Arjun <natharjun028@gmail.com>
2025-07-26 10:43:19 +0530

    feat : addition of profile controller to fetch user profile

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const{register , login ,getAllUsers} = require("../controllers/authController");
+const { register, login, getAllUsers, getUserProfile } = require("../controllers/authController");
+const { verifyToken } = require("../middleware/verifytoken");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users", getAllUsers);
+router.get("/profile", verifyToken, getUserProfile);
 
 module.exports = router;
