799e0f1e2174d6298d08e534ae39cdaddb87cb41
Arjun <natharjun028@gmail.com>
2025-07-28 23:19:17 +0530

    chore : rendering the app as a child of authcontext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import {  AuthContextProvider } from "./context/AuthContext"; // 👈 import this
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,19 +7,22 @@ import App from "./App";
 import "./index.css";
 import "./App.css";
 import "./font.css";
+// 👈 import this
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        toastOptions={{
-          className: 'backdrop-blur-md bg-black/80 text-white rounded-xl px-4 py-2 shadow-xl',
-          duration: 3000,
-        }}
-      />
+      < AuthContextProvider> {/* 👈 wrap the entire app */}
+        <App />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            className: 'backdrop-blur-md bg-black/80 text-white rounded-xl px-4 py-2 shadow-xl',
+            duration: 3000,
+          }}
+        />
+      </ AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
