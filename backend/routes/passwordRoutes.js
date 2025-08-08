ec12a01538e7ceb291de466d2c7c79950f79a550
Arjun <natharjun028@gmail.com>
2025-08-08 17:09:45 +0530

    chore : route updated for opt verification

--- backend/routes/passwordRoutes.js
+++ backend/routes/passwordRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   requestReset,
   resetPassword,
+  verifyOtp,
 } = require("../controllers/passwordController");
 router.post("/request-reset", requestReset);
+router.post("/verify-otp", verifyOtp);
 router.post("/reset-password", resetPassword);
 
 module.exports = router;
