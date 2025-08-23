0ebe79ff468b1cb465be376277f8fd676e285302
Arjun <natharjun028@gmail.com>
2025-08-23 10:50:06 +0530

    chore : updated the config file for shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import glsl from 'vite-plugin-glsl'
+import path from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss(), react() , glsl()],
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
