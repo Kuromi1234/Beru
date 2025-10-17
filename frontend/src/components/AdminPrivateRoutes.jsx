480843d804b7ac965fa5228b252ea6693ab006a0
Arjun <natharjun028@gmail.com>
2025-10-17 20:40:18 +0530

    feat: add spacing for improved readability in AdminPrivateRoutes component

--- frontend/src/components/AdminPrivateRoutes.jsx
+++ frontend/src/components/AdminPrivateRoutes.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 
+
 const AdminPrivateRoute = () => {
   const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user"));
